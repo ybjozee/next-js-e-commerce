008fef4b1993d9ddaeb106e17303660d31433a88
Joseph Udonsak <joseph.udonsak@live.co.uk>
2021-05-17 15:53:17 +0100

    Implementation of API endpoint to handle orders

---
+++ pages/api/order.js
@@ -0,0 +1,20 @@
+const getMessage = (cart, orderTotal) => {
+  const orderId = Math.random().toString(36).substring(2, 9);
+  return `Congratulations, your order (${orderId}) worth $${orderTotal} for ${cart.length} items has been processed successfully. The items will be delivered within 3 working days.`;
+};
+
+const handler = async (request, response) => {
+  switch (request.method) {
+    case "POST":
+      const { cart, orderTotal, phoneNumber } = request.body;
+      const message = getMessage(cart, orderTotal);
+      response.status(200).json({ message });
+      break;
+    default:
+      response
+        .status(405)
+        .end("This method is not allowed for this route.");
+  }
+};
+
+export default handler;
